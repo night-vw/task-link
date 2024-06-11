8bd845081568d9d526d33968bfa0513b723e86d3
night-vw <waw78075@gmail.com>
2024-06-11 20:38:52 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import dynamic from "next/dynamic";
 const TaskMap = async () => {
-  const supabase = createServerComponentClient({ cookies }) ;
-  const {data:user} = await supabase.auth.getSession();
-  const session = user.session;
-  console.log(session);
-
+  const Map = React.useMemo(
+    () =>
+      dynamic(() => import("@/components/Map"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
   return (
     <div className="text-2xl mt-100 mt-16 md:mt-0">
       <Map />
