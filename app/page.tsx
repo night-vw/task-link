2f9fa4940fac9e1092db57643b7f994c43a7d30a
night-vw <waw78075@gmail.com>
2024-06-12 11:48:56 +0900

    upgrade:MapLoading

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import dynamic from 'next/dynamic';
 
 // ローディングコンポーネント
 const Loading = () => (
-  <div className="flex justify-center items-center h-screen">
+  <div className="flex flex-col justify-center items-center h-screen">
     <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-indigo-600"></div>
+    <p className="mt-4 text-zinc-600">マップを読み込み中...</p>
   </div>
 );
+
 
 const TaskMap = () => {
   const Map = React.useMemo(
