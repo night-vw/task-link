d721d27cf7b7c0a7f610a59ac1de62358d243e26
night-vw <waw78075@gmail.com>
2024-06-13 12:03:53 +0900

    fix:SideMenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -86,8 +86,8 @@ const SideMenu = () => {
             </Link>
           </div>
           <ul className='mt-10'>
-            <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex hover:bg-gray-700 ${pathname == "/" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/" onClick={handleLinkClick} className='flex pr-20'><FaMapMarkedAlt className='pr-2' />Taskマップ</Link></li>
-            <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex hover:bg-gray-700 ${pathname == "/about" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/about" onClick={handleLinkClick} className='flex pr-28'><CiMemoPad className='pr-2' />Taskメモ</Link></li>
+            <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex hover:bg-gray-700 ${pathname == "/" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/" onClick={handleLinkClick} className='flex pr-10'><FaMapMarkedAlt className='pr-2' />Taskマップ</Link></li>
+            <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex hover:bg-gray-700 ${pathname == "/about" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/about" onClick={handleLinkClick} className='flex pr-20'><CiMemoPad className='pr-2' />Taskメモ</Link></li>
             <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex pr-30 hover:bg-gray-700 ${pathname == "/none_1" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/none_1" onClick={handleLinkClick} className='flex pr-30'><BiLoader className='pr-2' />None</Link></li>
             <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex hover:bg-gray-700 ${pathname == "/none_2" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/none_2" onClick={handleLinkClick} className='flex pr-30'><BiLoader className='pr-2' />None</Link></li>
             <li className={`-mr-4 -ml-5 py-3 pl-3 text-3xl flex hover:bg-gray-700 ${pathname == "/none_3" ? "bg-gray-600 border-r-4 border-r-indigo-300" : ""}`}><Link href="/none_3" onClick={handleLinkClick} className='flex pr-30'><BiLoader className='pr-2' />None</Link></li>
